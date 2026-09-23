74634f62e60d34d2678e8d2a730021d85f45b3c8
Constantine Saveliev <melinda.senoir@gmail.com>
2026-09-23 22:11:28 +0300

    feat: implement theme switching

--- coffee-house/src/main.js
+++ coffee-house/src/main.js
@@ -1,1 +1,17 @@
-import './style.scss';
+import "./style.scss";
+
+const themeSwitch = document.querySelector("#theme-switch");
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+  themeSwitch.checked = true;
+  document.documentElement.dataset.theme = "dark";
+}
+
+themeSwitch.addEventListener("change", () => {
+  const theme = themeSwitch.checked ? "dark" : "light";
+
+  document.documentElement.dataset.theme = theme;
+  localStorage.setItem("theme", theme);
+});
