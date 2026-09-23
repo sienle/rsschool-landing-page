7df5935d77484812ef1c56cd413b851fce48e406
Constantine Saveliev <melinda.senoir@gmail.com>
2026-09-24 01:09:06 +0300

    feat: implement burger menu

--- coffee-house/src/main.js
+++ coffee-house/src/main.js
@@ -30,3 +30,42 @@ if (scrollUp) {
     });
   });
 }
+
+
+//Burger
+const burgerButton = document.querySelector('.burger');
+const burgerMenu = document.querySelector('.burger__nav');
+const overflowWrapper = document.querySelector('.overflow__wraper');
+const menuLinks = document.querySelectorAll('.burger__nav-item');
+
+burgerButton.addEventListener('click', ()=> {
+  burgerButton.classList.toggle('burger__active');
+  burgerMenu.classList.toggle('burger__nav_active');
+  overflowWrapper.classList.toggle('overflow__wraper_active');
+  if (burgerButton.classList.contains('burger__active')) {
+    document.querySelector('body').style.overflow = 'hidden';
+  } else {
+    document.querySelector('body').style.overflow = null;
+  }
+});
+
+menuLinks.forEach(item => {
+  item.addEventListener('click', ()=> {
+    if (document.querySelector('body').style.overflow == 'hidden') {
+      burgerButton.classList.toggle('burger__active');
+      burgerMenu.classList.toggle('burger__nav_active');
+      overflowWrapper.classList.toggle('overflow__wraper_active');
+      document.querySelector('body').style.overflow = null;
+    }
+  });
+});
+
+window.addEventListener('resize', () => {
+  screenWid = window.innerWidth;
+  if (screenWid > 768 && burgerButton.classList.contains('burger__active')) {
+    burgerButton.classList.toggle('burger__active');
+    burgerMenu.classList.toggle('burger__nav_active');
+    overflowWrapper.classList.toggle('overflow__wraper_active');
+    document.querySelector('body').style.overflow = null;
+  }
+})
